d9518837f7cdb4b4b4ede00d433a84abb28b9bfa
MkDomain <mkdomainyt@gmail.com>
2020-11-23 08:51:14 +0100

    bug fix

--- src/main/java/me/mkdomain/discordmodereator/Main.java
+++ src/main/java/me/mkdomain/discordmodereator/Main.java
@@ -12,7 +12,7 @@ public class Main {
     private static final CommandManager manager = new CommandManager();
 
     public static void main(String[] args) throws LoginException {
-        JDABuilder builder = JDABuilder.createDefault("NzgwMTUxMzk0NDMzODkyMzgy.X7q6mg.9tlgdBmlZ5raJ1EQfZHYVRbevgw");
+        JDABuilder builder = JDABuilder.createDefault(args[0]);
         builder.setBulkDeleteSplittingEnabled(false);
         builder.setCompression(Compression.ZLIB);
         builder.setActivity(Activity.watching("8.A osztály"));
